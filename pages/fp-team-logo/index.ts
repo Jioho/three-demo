53b9c29afa0de41403a5bd3b9185980c3d3ddbbd
Jioho <jioho_chen@163.com>
2022-08-16 00:14:40 +0800

    调整

--- pages/fp-team-logo/index.ts
+++ pages/fp-team-logo/index.ts
@@ -21,13 +21,13 @@ window.addEventListener('resize', () => {
 const canvas = document.querySelector('.webgl')
 const params = {
   color: 0x522a0f,
-  metalness: 0.4,
+  metalness: 0.7,
   roughness: 0,
-  ambientColor: 0xffffff,
-  directionalColor: 0xffffff,
-  pointColor: 0xffffff
+  ambientColor: 0xfff7e6,
+  directionalColor: 0xe6cbcb,
+  pointColor: 0xfffe00
 }
-const gui = new dat.GUI()
+const gui = new dat.GUI({ closed: true })
 
 gui.addColor(params, 'color').onChange(() => {
   const color = new THREE.Color(params.color)
@@ -39,14 +39,20 @@ gui.addColor(params, 'ambientColor').onChange(() => {
   const color = new THREE.Color(params.ambientColor)
   ambientLight.color = color
 })
-gui.addColor(params, 'directionalColor').onChange(() => {
-  const color = new THREE.Color(params.directionalColor)
-  directionalLight.color = color
-}).name('直射光颜色')
-gui.addColor(params, 'pointColor').onChange(() => {
-  const color = new THREE.Color(params.pointColor)
-  pointLight.color = color
-}).name('点光颜色')
+gui
+  .addColor(params, 'directionalColor')
+  .onChange(() => {
+    const color = new THREE.Color(params.directionalColor)
+    directionalLight.color = color
+  })
+  .name('直射光颜色')
+gui
+  .addColor(params, 'pointColor')
+  .onChange(() => {
+    const color = new THREE.Color(params.pointColor)
+    pointLight.color = color
+  })
+  .name('点光颜色')
 
 gui.add(params, 'metalness', 0, 1, 0.1).onChange(() => {
   cube.material.metalness = params.metalness
@@ -61,18 +67,18 @@ gui.add(params, 'roughness', 0, 1, 0.1).onChange(() => {
 
 const scene = new THREE.Scene()
 
-const ambientLight = new THREE.AmbientLight(params.ambientColor, 0.9)
+const ambientLight = new THREE.AmbientLight(params.ambientColor, 2)
 scene.add(ambientLight)
 const ambientGroup = gui.addFolder('ambient')
 ambientGroup.add(ambientLight, 'intensity', 0, 50, 0.01)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 5.2)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3)
 
 const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
 directionalLightHelper.visible = false
-directionalLight.position.x = -6
+directionalLight.position.x = -5.1
 directionalLight.position.y = 5.3
-directionalLight.position.z = -0.4
+directionalLight.position.z = 1.1
 
 const directionalGroup = gui.addFolder('directional-light')
 directionalGroup.add(directionalLight.position, 'x', -10, 10, 0.1)
@@ -83,12 +89,11 @@ directionalGroup.add(directionalLightHelper, 'visible')
 
 scene.add(directionalLight, directionalLightHelper)
 
-const pointLight = new THREE.PointLight(params.pointColor, 2.66, -3.51)
-pointLight.position.set(-3.5, 0.02, 0)
+const pointLight = new THREE.PointLight(params.pointColor, 10, 4.3)
+pointLight.position.set(-2.85, 0.01, -0.43)
 scene.add(pointLight)
 
-const sphereSize = 1
-const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize)
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 1)
 pointLightHelper.visible = false
 
 scene.add(pointLightHelper)
@@ -101,6 +106,25 @@ pointGroup.add(pointLight.position, 'z', -10, 10, 0.01)
 pointGroup.add(pointLight, 'intensity', 0, 10, 0.01)
 pointGroup.add(pointLight, 'distance', -10, 10, 0.01)
 pointGroup.add(pointLightHelper, 'visible')
+
+const pointLight2 = new THREE.SpotLight(params.pointColor, 4.79, 1.56)
+pointLight2.position.set(0.68, 0, 0)
+
+scene.add(pointLight2)
+
+const pointLight2Helper = new THREE.SpotLightHelper(pointLight2, 1)
+pointLight2Helper.visible = false
+
+scene.add(pointLight2Helper)
+
+const point2Group = gui.addFolder('pointLight2')
+point2Group.add(pointLight2.position, 'x', -10, 10, 0.01)
+point2Group.add(pointLight2.position, 'y', -10, 10, 0.01)
+point2Group.add(pointLight2.position, 'z', -10, 10, 0.01)
+
+point2Group.add(pointLight2, 'intensity', 0, 10, 0.01)
+point2Group.add(pointLight2, 'distance', -10, 10, 0.01)
+point2Group.add(pointLight2Helper, 'visible')
 
 // 区域光
 // const reactAreaLight = new THREE.RectAreaLight(0xffffff, 10, 2, 2)
@@ -140,12 +164,16 @@ coneGroup.add(cone.rotation, 'x', -Math.PI * 2, Math.PI * 2, 0.01)
 cone.material.metalness = params.metalness
 scene.add(cone)
 
-const torus = new THREE.Mesh(new THREE.TorusBufferGeometry(0.45, 0.22, 32, 64), new THREE.MeshStandardMaterial({ color: params.color }))
-torus.position.x = 1.5
+const torus = new THREE.Mesh(new THREE.TorusBufferGeometry(0.5, 0.26, 32, 64), new THREE.MeshStandardMaterial({ color: params.color }))
+torus.position.x = 1.4
+torus.position.z = -0.5
 torus.material.metalness = params.metalness
 
-torus.rotation.x = -0.14
-torus.rotation.y = -0.55
+pointLight2.target = torus
+scene.add(pointLight2.target)
+
+torus.rotation.x = -0.41
+torus.rotation.y = -0.41
 const torusGroup = gui.addFolder('torus')
 torusGroup.add(torus.rotation, 'x', -Math.PI * 2, Math.PI * 2, 0.01)
 torusGroup.add(torus.rotation, 'y', -Math.PI * 2, Math.PI * 2, 0.01)
